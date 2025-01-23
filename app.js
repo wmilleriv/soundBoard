e9d3d043ff0dd0da4c8c45a5ec48d336a1f87139
William Miller <Wmilleiv@gmail.com>
2025-01-23 13:31:25 -0500

    changed keycode to key

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 window.addEventListener('keydown', function(e){
-   const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
-   const key=document.querySelector(`.key[data-key="${e.keyCode}"]`);
+   console.log(e);
+   const audio=document.querySelector(`audio[data-key="${e.key}"]`);
+   const key=document.querySelector(`.key[data-key="${e.key}"]`);
    if(!audio) return;
    audio.currentTime=0;
    audio.play();
