42bdfbf7c3fab70f69d3f0466d0c0d4a0fe8d471
William Miller <Wmilleiv@gmail.com>
2025-01-24 13:55:55 -0500

    Changed anonymous function style

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-window.addEventListener('keydown', function(e){
+window.addEventListener('keydown', (e)=>{
    console.log(e);
    const audio=document.querySelector(`audio[data-key="${e.key}"]`);
    const key=document.querySelector(`.key[data-key="${e.key}"]`);
